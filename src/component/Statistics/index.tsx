3287472c43c35e21f49d6f97cc3ad3a370c54f4e
limyeonsang <limquo@gmail.com>
2022-05-23 00:01:29 +0900

    Add: nivo chart

--- src/component/Statistics/index.tsx
+++ src/component/Statistics/index.tsx
@@ -1,28 +1,14 @@
+import { DepressionExpRate } from "../DepressionExpRate"
 import * as S from "./styles"
+import depressionExperienceRate from "../../asset/chartData/depression_experience_rate.json"
 
 const Statistics = () => {
   return (
     <S.StatisticsBox>
-      <S.YearlyTrend>
-        <g>
-          <S.YAxisText x="24" y="14" textAnchor="end">8%</S.YAxisText>
-          <S.YAxisText x="24" y="91" textAnchor="end">6%</S.YAxisText>
-          <S.YAxisText x="24" y="167" textAnchor="end">4%</S.YAxisText>
-        </g>
-        <g>
-          <S.XAxisText x="71" y="182" textAnchor="middle">2015년</S.XAxisText>
-          <S.XAxisText x="223.5" y="182" textAnchor="middle">2016년</S.XAxisText>
-          <S.XAxisText x="376" y="182" textAnchor="middle">2017년</S.XAxisText>
-          <S.XAxisText x="528.5" y="182" textAnchor="middle">2018년</S.XAxisText>
-          <S.XAxisText x="681" y="182" textAnchor="middle">2019년</S.XAxisText>
-          <S.XAxisText x="844" y="182" textAnchor="middle">2020년</S.XAxisText>
-        </g>
-        <g aria-hidden="true">
-          <path stroke="white" strokeWidth="1" d="M 39 9.5 L 876 9.5"></path>
-          <path stroke="white" strokeWidth="1" d="M 39 87.5 L 876 87.5"></path>
-          <path stroke="white" strokeWidth="1" d="M 39 163.5 L 876 163.5"></path>
-        </g>
-      </S.YearlyTrend>
+      <div style={{ width: 800, height: 300 }}>
+        {/* <DepressionExpRate data={data}/> */}
+        <DepressionExpRate data={depressionExperienceRate}/>
+      </div>
     </S.StatisticsBox>
   )
 }
--- src/component/Statistics/styles.ts
+++ src/component/Statistics/styles.ts
@@ -5,22 +5,3 @@ export const StatisticsBox = styled.section`
   justify-content: center;
   align-items: center;
 `
-
-export const YearlyTrend = styled.svg`
-  width:886px; 
-  height:200px;
-`
-
-export const XAxisText = styled.text`
-  fill: white;
-  font-size: 13px;
-`
-
-export const YAxisText = styled.text`
-  fill: white;
-  font-size: 15px;
-`
-
-export const DataText = styled.text`
-  fill: white;
-`
